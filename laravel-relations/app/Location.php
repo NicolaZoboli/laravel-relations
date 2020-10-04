830eeba5736c88f37d72be74ab87ddbac23282b8
Nicola Zoboli <64474850+NicolaZoboli@users.noreply.github.com>
2020-10-04 19:19:25 +0200

    Completato esercizio del 01/10

---
+++ laravel-relations/app/Location.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+
+  protected $fillable = [
+
+    'name',
+    'street',
+    'city',
+    'state'
+  ];
+
+  public function employees() {
+
+    return $this -> hasMany(Employee::class);
+  }
+}
